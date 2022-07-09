0b4420228fb895d0a580cc270dda8ccc395cfab6
miguelalonsocoba <miguetheking53@gmail.com>
2022-07-09 09:52:07 -0500

    Se comento el atributo Environment

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +14,10 @@ import com.formacionbdi.springboot.app.productos.models.service.IProductoService
 
 @RestController
 public class ProductoController {
-	
-	@Autowired
-	private Environment env;
-	
+
+//	@Autowired
+//	private Environment env;
+
 	@Value("${server.port}")
 	private Integer port;
 
